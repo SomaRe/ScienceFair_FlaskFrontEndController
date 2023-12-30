9c6901c0d054e363dbc186447c4bf285676ecba7
soma <shivatej82@gmail.com>
2023-12-30 16:04:26 -0500

    whisper force english, clock 12-0 fix

--- app.py
+++ app.py
@@ -53,17 +53,17 @@ def start():
     words = ()
     chosen_word = ""
     clock = {}
-    # instruction = "I will ask you some questions, and give you some problems to solve. Please answer them to the best of your ability."
-    # speak(instruction)
-    # time.sleep(1)
+    instruction = "I will ask you some questions, and give you some problems to solve. Please answer them to the best of your ability."
+    speak(instruction)
+    time.sleep(1)
 
     questions = [
-        # "What is the current year?",
-        # "what is the current season?",
-        # "What is the date today?",
-        # "What is the current day of the week?",
-        # "What is the current month?",
-        # "What country are we in?",
+        "What is the current year?",
+        "what is the current season?",
+        "What is the date today?",
+        "What is the current day of the week?",
+        "What is the current month?",
+        "What country are we in?",
         # "What county are we in?",
         # "What city are we in?",
     #     "What is the name or address of this building?",
@@ -79,55 +79,55 @@ def start():
 
 @app.route('/group2', methods=['POST', 'GET'])
 def group2():
-    # global three_things
+    global three_things
 
-    # instruction = 'I am going to name 3 objects. After I have said them I want you to repeat them back to me'
-    # speak(instruction)
-    # time.sleep(2)
+    instruction = 'I am going to name 3 objects. After I have said them I want you to repeat them back to me'
+    speak(instruction)
+    time.sleep(2)
 
-    # three_things = test_questions.get_three_things()
-    # speak(three_things[0] + '. ' + three_things[1] + '. And ' + three_things[2])
+    three_things = test_questions.get_three_things()
+    speak(three_things[0] + '. ' + three_things[1] + '. And ' + three_things[2])
 
-    # spoken_text = text_and_speech.convert_speech_to_text()
-    # data['three_things'] = {
-    #     'spoken_text': spoken_text,
-    #     'correct': " ".join(three_things)
-    # }
-    # time.sleep(1)
-    # speak("Repeat them few more times and try to remember, I will ask you to repeat them in few minutes.")
-    # time.sleep(10)
+    spoken_text = text_and_speech.convert_speech_to_text()
+    data['three_things'] = {
+        'spoken_text': spoken_text,
+        'correct': " ".join(three_things)
+    }
+    time.sleep(1)
+    speak("Repeat them few more times and try to remember, I will ask you to repeat them in few minutes.")
+    time.sleep(10)
 
 
     return jsonify(data)
 
 @app.route('/group3', methods=['POST', 'GET'])
 def group3():
-    # instruction = "Now I am going to give you a word, and I want you to say it backwards."
-    # speak(instruction)
-    # time.sleep(1)
-    # word = test_questions.get_reverse_word()
-    # speak("The word is: " + word)
+    instruction = "Now I am going to give you a word, and I want you to say it backwards."
+    speak(instruction)
+    time.sleep(1)
+    word = test_questions.get_reverse_word()
+    speak("The word is: " + word)
 
-    # spoken_text = text_and_speech.convert_speech_to_text(model = "google")
-    # print(spoken_text)
-    # data['reverse_word'] = {
-    #     'spoken_text': spoken_text.replace(" ", ""),
-    #     'correct': word[::-1]
-    # }
+    spoken_text = text_and_speech.convert_speech_to_text()
+    print(spoken_text)
+    data['reverse_word'] = {
+        'spoken_text': spoken_text.replace(" ", ""),
+        'correct': word[::-1]
+    }
 
     return jsonify(data)
 
 @app.route('/group4', methods=['POST', 'GET'])
 def group4():
-    # instructions = "Can you repeat the three things I asked you to remember earlier?"
-    # speak(instructions)
-    # time.sleep(1)
+    instructions = "Can you repeat the three things I asked you to remember earlier?"
+    speak(instructions)
+    time.sleep(1)
 
-    # spoken_text = text_and_speech.convert_speech_to_text()
-    # data['three_things_repeat'] = {
-    #     'spoken_text': spoken_text,
-    #     'correct': " ".join(three_things)
-    # }
+    spoken_text = text_and_speech.convert_speech_to_text()
+    data['three_things_repeat'] = {
+        'spoken_text': spoken_text,
+        'correct': " ".join(three_things)
+    }
 
     return jsonify(data)
 
@@ -140,15 +140,15 @@ def group5_1():
 
 @app.route('/group5_2', methods=['POST', 'GET'])
 def group5_2():
-    # instructions = "Name the object in the picture."
-    # speak(instructions)
-    # time.sleep(1)
+    instructions = "Name the object in the picture."
+    speak(instructions)
+    time.sleep(1)
 
-    # spoken_text = text_and_speech.convert_speech_to_text()
-    # data['image1'] = {
-    #     'spoken_text': spoken_text,
-    #     'correct': image_data1[0]
-    # }
+    spoken_text = text_and_speech.convert_speech_to_text()
+    data['image1'] = {
+        'spoken_text': spoken_text,
+        'correct': image_data1[0]
+    }
 
     return jsonify(data)
 
@@ -165,28 +165,28 @@ def group5_1_repeat():
 
 @app.route('/group5_2_repeat', methods=['POST', 'GET'])
 def group5_2_repeat():
-    # instructions = "Now name this object"
-    # speak(instructions)
-    # time.sleep(1)
+    instructions = "Now name this object"
+    speak(instructions)
+    time.sleep(1)
 
-    # spoken_text = text_and_speech.convert_speech_to_text()
-    # data['image2'] = {
-    #     'spoken_text': spoken_text,
-    #     'correct': image_data2[0]
-    # }
+    spoken_text = text_and_speech.convert_speech_to_text()
+    data['image2'] = {
+        'spoken_text': spoken_text,
+        'correct': image_data2[0]
+    }
 
     return jsonify(data)
 
 @app.route('/group6', methods=['POST', 'GET'])
 def group6():
-    # instructions = "I would like you to repeat the following phrase after me: No ifs, ands, or buts."
-    # speak(instructions)
+    instructions = "I would like you to repeat the following phrase after me: No ifs, ands, or buts."
+    speak(instructions)
 
-    # spoken_text = text_and_speech.convert_speech_to_text()
-    # data['phrase'] = {
-    #     'spoken_text': spoken_text,
-    #     'correct': "No ifs, ands, or buts"
-    # }
+    spoken_text = text_and_speech.convert_speech_to_text()
+    data['phrase'] = {
+        'spoken_text': spoken_text,
+        'correct': "No ifs, ands, or buts"
+    }
 
     return jsonify(data)
 
@@ -194,8 +194,8 @@ def group6():
 @app.route('/group7_1', methods=['POST', 'GET'])
 def group7_1():
     global words, chosen_word
-    # instructions = "Read the instructions on the screen and do what it says."
-    # speak(instructions)
+    instructions = "Read the instructions on the screen and do what it says."
+    speak(instructions)
 
     words = test_questions.get_words_to_click()
     chosen_word = random.choice(words)
@@ -230,7 +230,7 @@ def group8_1():
 def group8_2():
     if request.method == 'POST':
         r = request.get_json()
-        r['hour'] = int(r['hour'])//30
+        r['hour'] = 12 if int(r['hour'])//30 == 0 else int(r['hour'])//30
         r['minute'] = int(r['minute'])//6
 
         data['clock'] = {
@@ -238,6 +238,7 @@ def group8_2():
             'correct': clock
         }
 
+    save_data_to_file()
     return jsonify(data)
 
 
--- text_and_speech.py
+++ text_and_speech.py
@@ -44,7 +44,7 @@ def convert_speech_to_text(model = "whisper"):
                         continue
                     return text['text']
                 elif model == "whisper":
-                    text = r.recognize_whisper(audio)
+                    text = r.recognize_whisper(audio, language="en")
                     print(text)
                     if text == "":
                         repeat = True
